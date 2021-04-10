2f9b5c96720857eeaadec03523960fcd2a2dc486
Yvette Kondoh <yvetteekon@gmail.com>
2021-04-10 00:51:11 -0700

    Create churn data

--- codes/filepaths.py
+++ codes/filepaths.py
@@ -16,11 +16,13 @@ raw_products_data = os.path.join(RAW_DIR, 'prod_cat_info.csv')
 
 #interim data files
 interim_transactions_data = os.path.join(INTERIM_DIR, 'transactions_data_clean.csv')
+interim_transactions_data_aggregated = os.path.join(INTERIM_DIR, 'transactions_data_aggregated.csv')
 
 #processed data files
 master_file_data = os.path.join(PROCESSED_DIR, 'master_file.csv')
 purchase_frequency_distribution = os.path.join(PROCESSED_DIR, 'purchase_frequency_distribution.csv')
 processed_transactions_data = os.path.join(PROCESSED_DIR, 'transactions_data_final.csv')
+processed_churn_data =  os.path.join(PROCESSED_DIR, 'churn_data.csv')
 
 #dictionary
 dictionary_file = os.path.join(DICTIONARY_DIR, 'nomenclature_dictionary.xlsx')
